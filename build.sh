345f4e77fd6310213c5a7c43aeef534808a72f51
Brian Heise <bnheise@gmail.com>
2025-10-11 13:49:27 +0900

    switch to musl build of zola

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 ZOLA_VERSION=0.21.0
 
 echo "Installing Zola ${ZOLA_VERSION}..."
-curl -L "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz" -o zola.tar.gz
+curl -L "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-musl" -o zola.tar.gz
 tar -xzf zola.tar.gz
 mv zola /usr/local/bin/zola
 zola --version
